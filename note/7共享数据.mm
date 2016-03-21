743f734abbe7a85843c4d8ec1c745c4096019b9c
cinhori <lleers32@gmail.com>
2016-03-21 23:19:21 +0800

    add ContentProvider

---
+++ note/7共享数据.mm
@@ -0,0 +1,17 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1458570161913" ID="ID_36506078" MODIFIED="1458570192213" TEXT="7&#x5171;&#x4eab;&#x6570;&#x636e;">
+<node CREATED="1458570267830" ID="ID_1172608612" MODIFIED="1458570283042" POSITION="right" TEXT="&#x8bbf;&#x95ee;&#x5176;&#x4ed6;&#x7a0b;&#x5e8f;&#x4e2d;&#x7684;&#x6570;&#x636e;">
+<node CREATED="1458570842143" ID="ID_270361767" MODIFIED="1458570891681" TEXT="ContentResolver&#x7c7b;&#xff1a;&#x901a;&#x8fc7;getContentResolver()&#x83b7;&#x5f97;"/>
+<node CREATED="1458570908052" ID="ID_668978004" MODIFIED="1458571685201" TEXT="&#x5185;&#x5bb9;URI&#xff1a;anthority/path/(id)+ Uri.parse()"/>
+<node CREATED="1458571021844" ID="ID_272311705" MODIFIED="1458571026121" TEXT="CRUD"/>
+</node>
+<node CREATED="1458571060967" ID="ID_1199763961" MODIFIED="1458571074879" POSITION="left" TEXT="&#x521b;&#x5efa;&#x5185;&#x5bb9;&#x63d0;&#x4f9b;&#x5668;">
+<node CREATED="1458571420009" ID="ID_245502742" MODIFIED="1458571485492" TEXT="1&#x7ee7;&#x627f;ContentProvider&#xff0c;&#x5b9e;&#x73b0;6&#x62bd;&#x8c61;&#x65b9;&#x6cd5;">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1458571738674" ID="ID_1175770224" MODIFIED="1458572037385" TEXT="2Urimatcher/addURI(authority+path+id)"/>
+<node CREATED="1458572275280" ID="ID_635647463" MODIFIED="1458572379113" TEXT="3&#x6ce8;&#x518c;&#xff1a;&lt;provider...&gt;"/>
+</node>
+</node>
+</map>
