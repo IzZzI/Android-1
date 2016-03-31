05a000279e526950347e11e5e1639a4a25520493
cinhori <lleers32@gmail.com>
2016-03-31 22:22:29 +0800

    add code of Sensor

---
+++ code/12Sensor/CompassTest/src/com/example/compasstest/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.compasstest;
+
+import javax.security.auth.PrivateCredentialPermission;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+	private SensorManager sensorManager;
+	private ImageView compassImg;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		compassImg = (ImageView)findViewById(R.id.compass_img);
+		sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+		Sensor accelerometerSensor = sensorManager.getDefaultSensor
+				(Sensor.TYPE_ACCELEROMETER);
+		Sensor magneticsSensor = sensorManager.getDefaultSensor
+				(Sensor.TYPE_MAGNETIC_FIELD);
+		sensorManager.registerListener
+			(listener, magneticsSensor, SensorManager.SENSOR_DELAY_GAME);
+		sensorManager.registerListener
+			(listener, accelerometerSensor, sensorManager.SENSOR_DELAY_GAME);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		if(sensorManager != null){
+			sensorManager.unregisterListener(listener);
+		}
+	}
+	
+	private SensorEventListener listener = new SensorEventListener() {
+		
+		float[] accelerometerValues = new float[3];
+		float[] magneticValues = new float[3];
+		private float lastRotateDegree;
+		
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			// TODO Auto-generated method stub
+			if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+				accelerometerValues = event.values.clone();
+			}else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+				magneticValues = event.values.clone();
+			}
+			float[]	R = new float[9];
+			float[] values = new float[3];
+			SensorManager.getRotationMatrix
+				(R, null, accelerometerValues, magneticValues);
+			SensorManager.getOrientation(R, values);
+			//Ðý×ª·½Ïò
+			float rotateDegree = -(float)Math.toDegrees(values[0]);
+			if(Math.abs(rotateDegree - lastRotateDegree) > 1){
+				RotateAnimation animation = new RotateAnimation(lastRotateDegree, rotateDegree, 
+						Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+				animation.setFillAfter(true);
+				compassImg.startAnimation(animation);
+				lastRotateDegree = rotateDegree;
+			}
+			
+			Log.d("MainActivity", "value[0] is " + Math.toDegrees(values[0]));
+		}
+		
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
